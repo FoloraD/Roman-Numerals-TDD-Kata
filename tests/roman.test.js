85ed2d4e9c1a57969711cb2ce392184723c4ee57
folorad <fduang@gmail.com>
2023-08-07 13:05:26 +0100

    Add tests for 2 digit and 3 digit intergers

--- tests/roman.test.js
+++ tests/roman.test.js
@@ -11,6 +11,18 @@ describe('Roman Numeral converter', () => {
     test('input 5 should return V', () => {
         expect(numberToRoman(5)).toEqual('V')
    });
+
+   test('input 9 should return IX', () => {
+    expect(numberToRoman(9)).toEqual('IX')
+   });
+
+   test('input 64 should return LXIV', () => {
+    expect(numberToRoman(64)).toEqual('LXIV')
+   });
+
+   test('input 789 should return DCCLXXXIX', () => {
+    expect(numberToRoman(789)).toEqual('DCCLXXXIX')
+   });
 });
 
 // input 5 => 'V'
